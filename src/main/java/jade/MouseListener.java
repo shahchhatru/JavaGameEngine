9a25efe0d25de06200305561b6c7bad6a3a94b18
shahchhatru <shahkhesavchhatra@gmail.com>
2024-08-10 06:31:41 +0545

    Window class created

---
+++ src/main/java/jade/MouseListener.java
@@ -0,0 +1,12 @@
+package jade;
+
+public class MouseListener {
+    private static MouseListener instance;
+    private double scrollX, scrollY;
+    private double xPos, yPos, lastX, lastY;
+    private boolean mouseButtonPressed[] = new boolean[3];
+  //  private boolean isDragging
+
+
+
+}
